079e3c02da7082eb4decb6d72f63e2d8b2332f88
paulomarchon <paulomarchon@gmail.com>
2025-05-21 11:23:37 -0300

    feat: Implementing the Repository pattern for Image Metadata

---
+++ Tempic/Interfaces/IImageMetadataRepository.cs
@@ -0,0 +1,12 @@
+﻿using Tempic.Models;
+
+namespace Tempic.Interfaces
+{
+    public interface IImageMetadataRepository : IDisposable 
+    {
+        ImageMetadata GetImageMetadataByUniqueLinkId(Guid uniqueLinkId);
+        void InsertImageMetadata(ImageMetadata imageMetadata);
+        void DeleteImageMetadata(Guid uniqueLinkId);
+        void SaveChanges();
+    }
+}
---
+++ Tempic/Interfaces/ImageMetadataRepository.cs
@@ -0,0 +1,53 @@
+﻿using Tempic.Data;
+using Tempic.Exceptions;
+using Tempic.Models;
+
+namespace Tempic.Interfaces
+{
+    public class ImageMetadataRepository : IImageMetadataRepository, IDisposable
+    {
+        private AppDbContext context;
+        public ImageMetadataRepository(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public ImageMetadata GetImageMetadataByUniqueLinkId(Guid uniqueLinkId)
+        {
+            return context.ImageMetadata.FirstOrDefault(x => x.UniqueLinkId == uniqueLinkId) ?? throw new Exception("Image not found");
+        }
+        public void InsertImageMetadata(ImageMetadata imageMetadata)
+        {
+            context.ImageMetadata.Add(imageMetadata);
+        }
+        public void DeleteImageMetadata(Guid uniqueLinkId)
+        {
+            context.ImageMetadata.Remove(context.ImageMetadata.FirstOrDefault(x => x.UniqueLinkId == uniqueLinkId) ?? throw new Exception("Image not found")
+);
+        }
+        public void SaveChanges()
+        {
+            context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
